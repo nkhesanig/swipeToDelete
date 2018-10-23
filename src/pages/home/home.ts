13f78a3258233589d58c795a7a9b134bf4d72018
nkhesani <nkhesani.mukoveni@gmail.com>
2018-10-23 14:16:26 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  list = [
+    "khensani",
+    "fresy",
+    "musa",
+    "andani"
+  ];
   constructor(public navCtrl: NavController) {
 
+  }
+
+  delete(i){
+    this.list.splice(i,1);
   }
 
 }
